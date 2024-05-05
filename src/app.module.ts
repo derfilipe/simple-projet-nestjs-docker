dce0d69d7cef14e130093bc331bb27e26a5ab51c
derfilipe <f.andrade@planerio.de>
2024-05-05 17:13:26 +0200

    Enabled cors, created .env.example file

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,14 +1,17 @@
-import { Module } from "@nestjs/common";
-import { AppController } from "./app.controller";
-import { AppService } from "./app.service";
-import { MongooseModule } from "@nestjs/mongoose";
-import { MenusModule } from "./menus/menus.module";
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { MongooseModule } from '@nestjs/mongoose';
+import { MenusModule } from './menus/menus.module';
 import { ClientsModule } from './clients/clients.module';
 
 @Module({
-  imports: [MongooseModule.forRoot(process.env.MONGODB_URL), MenusModule, ClientsModule],
+  imports: [
+    MongooseModule.forRoot(process.env.MONGODB_URL),
+    MenusModule,
+    ClientsModule,
+  ],
   controllers: [AppController],
-  providers: [AppService]
+  providers: [AppService],
 })
-export class AppModule {
-}
+export class AppModule {}
